2fc0d9f8e03e9064ebf2df440a923da8d3f2d4c6
Nathaniel Ho <nxho93@gmail.com>
2019-12-10 15:15:05 -0800

    Add website under construction note

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 			<h1>Nathaniel Ho</h1>
 			<h3>Current Projects</h3>
 			<a href='https://play-mahjong.netlify.com/'>Play some Mahjong!</a>
+			<p>*This website is still under construction (if you haven't noticed)*</p>
 		</div>
 	);
 }
